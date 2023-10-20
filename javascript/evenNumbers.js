ae493c0c354bdff2917c9f7ca9948db53c57abf6
Breanna Young <w0459556@campus.nscc.ca>
2023-10-20 09:19:38 -0300

    chaged loops to print 0 and 100

--- javascript/evenNumbers.js
+++ javascript/evenNumbers.js
@@ -10,11 +10,22 @@ function main()
 {
     let counter = 0;
     let strOutput = "";
-    while (counter<101){ 
+    // while loops
+    while (counter<=100){ 
         strOutput = strOutput + (counter) + ", ";
         counter = counter + 2;
     }
-    console.log(strOutput); // this will print the output all in one line 
+    console.log(strOutput); // this will print the output all in one line
+    
+
+    strOutput = ""
+    // for loops
+    for(let counter = 0; counter <= 100; counter = counter +1){
+        if (counter%2 == 0){
+            strOutput = strOutput + counter + ", ";
+        }
+        console.log(strOutput)
+    }
 
 }
 
